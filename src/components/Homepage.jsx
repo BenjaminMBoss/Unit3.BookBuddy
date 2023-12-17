76b6343fc25a87789538c0bd69175560f2fc9a29
Benjamin <benjamin.boss@icloud.com>
2023-12-17 10:33:56 -0500

    "adding in styling"

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -4,13 +4,13 @@ const Homepage = ({token}) => {
             {
                 token !== null ? 
                 <div>
-                    <h2>Welcome to our library!</h2>
+                    <h1>Welcome to our library!</h1>
                     <h4>Thank you for logging in! </h4>
                     <p>Check out any of our available books or return one of your checked out books.</p>
                 </div>
                 :
                 <div>
-                    <h2>Welcome to our library!</h2>
+                    <h1>Welcome to our library!</h1>
                     <h4>Feel free to browse our books in our library! To check out a book, please log in or create an account.</h4>
                 </div>
             }
